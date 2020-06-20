761c0a4af3f8f15a7ce58dcbc3ae2a04089e0c9e
Filipe Ataide (Hovione <fataide@hovione.com>
2020-06-20 23:22:26 +0100

    Country-level data

--- app.R
+++ app.R
@@ -23,12 +23,34 @@ todayCouncil=Council_Cases$data[length(Council_Cases$data)]
 todayCouncil=as.Date(todayCouncil,"%d-%m-%y")
 
 Region_Cases <- read.csv(text=fileRegions)
+Region_Deaths = Region_Cases[,c("data","obitos_arsnorte","obitos_arscentro","obitos_arslvt","obitos_arsalentejo","obitos_arsalgarve","obitos_acores","obitos_madeira")]
+Region_Recovered = Region_Cases[,c("data","recuperados_arsnorte","recuperados_arscentro","recuperados_arslvt","recuperados_arsalentejo","recuperados_arsalgarve","recuperados_acores","recuperados_madeira")]
 Region_Cases = Region_Cases[,c("data","confirmados_arsnorte","confirmados_arscentro","confirmados_arslvt","confirmados_arsalentejo","confirmados_arsalgarve","confirmados_acores","confirmados_madeira")]
 Regionlist = c("Norte","Centro","Lisboa e Vale do Tejo","Alentejo","Algarve","Acores","Madeira")
 colnames(Region_Cases)=c("data",Regionlist)
+colnames(Region_Deaths)=c("data",Regionlist)
+colnames(Region_Recovered)=c("data",Regionlist)
+
+Country_Cases <- read.csv(text=fileRegions)
+Country_Deaths = Country_Cases[,c("data","obitos")]
+Country_Recovered = Country_Cases[,c("data","recuperados")]
+Country_Cases = Country_Cases[,c("data","confirmados")]
+Countrylist = c("Portugal")
+colnames(Country_Cases)=c("data",Countrylist)
+colnames(Country_Deaths)=c("data",Countrylist)
+colnames(Country_Recovered)=c("data",Countrylist)
+
+Country_Active = Country_Cases-Country_Deaths-Country_Recovered
+Country_Active$data=Country_Cases$data
+
 todayRegion=Region_Cases$data[length(Region_Cases$data)]
 todayRegion=as.Date(todayRegion,"%d-%m-%y")
-  
+
+confinamentostart = "18-03-2020"
+confinamentostart = as.Date(confinamentostart,"%d-%m-%y")
+confinamentoend = "03-05-2020"
+confinamentoend = as.Date(confinamentoend,"%d-%m-%y")
+
 # Multiple plot function
 #
 # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
@@ -87,8 +109,19 @@ ui <- dashboardPage(
               tabBox(width = 12,
                      id="tab1",
                      tabPanel(
+                       "Country data",
+                       selectInput("type_case_country", "Type:",
+                                   c("Total","Deaths","Active"),
+                                   selected = "Total"),
+                       plotOutput("newcasesplot_country", height = "600px"),
+                       paste0("Source: https://github.com/dssg-pt/covid19pt-data (last update: ",todayRegion,")\n Inspiration from @yaneerbaryam")
+                     ),
+                     tabPanel(
                        "Region data",
-                       htmlOutput("legend1"),
+                       textOutput("legend1"),
+                       selectInput("type_case", "Type:",
+                                   c("Total","Deaths"),
+                                   selected = "Total"),
                        plotOutput("newcasesplot_region", height = "600px"),
                        paste0("Source: https://github.com/dssg-pt/covid19pt-data (last update: ",todayRegion,")\n Inspiration from @yaneerbaryam")
                      ),
@@ -110,7 +143,8 @@ ui <- dashboardPage(
 server <- function(input, output) {
   
   output$legend1 = renderText({
-    paste0("Daily cases below 10 - green;","\nDaily cases below 20 or below 20% of peak - yellow;","\n anything else - red")})
+    paste0("Daily cases below 10 - green; Daily cases below 20 or below 20% of peak - yellow\n anything else - red")
+  })
   
   ####Councils plot####
   observeEvent(input$location_council,{
@@ -146,28 +180,125 @@ server <- function(input, output) {
     output$newcasesplot_council <- renderPlot({
       p <- ggplot(plotdata_council,aes(x=date)) +
         geom_line(aes(y=smooth_newcases), size = 2, color=colour) +
-        # geom_point(aes(y=newcases), size = 3) +
         ggtitle(paste0(location," (total cases: ",TotalInfected[length(TotalInfected)],")")) +
-        xlab ("") + ylab("Daily cases") + 
-        theme(axis.text=element_text(size=14,face="bold"),
-              axis.title=element_text(size=16,face="bold"),
+        xlab ("") + ylab("Daily cases") + ylim(0, max(smooth_newcases,5)) +
+        theme(axis.text=element_text(face="bold"),
+              axis.title=element_text(face="bold"),
               aspect.ratio = 1/1.618,
-              plot.title = element_text(size = 20, face = "bold"))
+              plot.title = element_text(face = "bold"))
       p
     })
   })
   
   ####Regions plot####
-  # observeEvent(input$location_region,{
-  
-  maxnewcases= 0
-  for (k in Regionlist){
-    location = k
-    Region_Cases_k = Region_Cases[,c("data",location)]
-    date=Region_Cases_k$data
+  observeEvent(input$type_case,{
+    
     # print(Region_Cases_k)
+    maxnewcases= 0
+    for (k in Regionlist){
+      location = k
+      if (input$type_case == "Total"){
+        Region_Cases_k = Region_Cases[,c("data",location)]
+      } else if(input$type_case == "Deaths"){
+        Region_Cases_k = Region_Deaths[,c("data",location)]
+      }
+      
+      date=Region_Cases_k$data
+      date=as.Date(x=date,"%d-%m-%y")
+      TotalInfected = Region_Cases_k[,c(location)]
+      newcases=TotalInfected
+      smooth_newcases=newcases
+      smooth_newcases_2=newcases
+      rollingaverage=7
+      newcases[1]=0
+      smooth_newcases[1]=0
+      
+      for (i in 1:(length(TotalInfected)-1)){
+        newcases[i+1] = max(TotalInfected[i+1]-TotalInfected[i],0)
+        newcases[is.na(newcases)]=0
+        newcases[newcases<0]=0
+        smooth_newcases[i+1] = max((TotalInfected[i+1]-TotalInfected[max(1,i-rollingaverage-1)])/min(i,rollingaverage),0)
+      }
+      maxnewcases=max(maxnewcases,smooth_newcases)
+      
+      
+      plotdata_region=data.frame(date,smooth_newcases,newcases)
+      
+      
+      
+      
+      if (input$type_case == "Total"){
+        colour = if(smooth_newcases[length(smooth_newcases)] < 10){
+          "#52854C"
+        } else if(smooth_newcases[length(smooth_newcases)]/max(smooth_newcases)<0.2 || smooth_newcases[length(smooth_newcases)] < 20){
+          "#E7B800"
+        } else {
+          "#FC4E07"
+        }
+        p <- ggplot(plotdata_region,aes(x=date)) +
+          geom_line(aes(y=smooth_newcases), size = 2, color=colour) +
+          ggtitle(paste0(location," (total cases: ",TotalInfected[length(TotalInfected)],")")) +
+          xlab ("") + ylab("Daily cases") + ylim(0, max(smooth_newcases,10)) +
+          theme(axis.text=element_text(face="bold"),
+                axis.title=element_text(face="bold"),
+                aspect.ratio = 1/1.618,
+                plot.title = element_text(face = "bold"))
+      } else if(input$type_case == "Deaths"){
+        colour = if(smooth_newcases[length(smooth_newcases)] < 1){
+          "#52854C"
+        } else if(smooth_newcases[length(smooth_newcases)] < 2){
+          "#E7B800"
+        } else {
+          "#FC4E07"
+        }
+        p <- ggplot(plotdata_region,aes(x=date)) +
+          geom_line(aes(y=smooth_newcases), size = 2, color=colour) +ggtitle(paste0(location," (total deaths: ",TotalInfected[length(TotalInfected)],")")) +
+          xlab ("") + ylab("Daily deaths") + ylim(0, max(smooth_newcases,1)) +
+          theme(axis.text=element_text(face="bold"),
+                axis.title=element_text(face="bold"),
+                aspect.ratio = 1/1.618,
+                plot.title = element_text(face = "bold"))
+      }
+      
+      if (k == "Norte"){
+        p1 = p
+      } else if (k == "Centro"){
+        p2 = p
+      } else if (k == "Lisboa e Vale do Tejo"){
+        p3 = p
+      } else if (k == "Alentejo"){
+        p4 = p
+      } else if (k == "Algarve"){
+        p5 = p
+      } else if (k == "Acores"){
+        p6 = p
+      } else if (k == "Madeira"){
+        p7 = p
+      }
+    }
+    output$newcasesplot_region <- renderPlot({
+      multiplot(p1, p2, p3, p4, p5, p6, p7, cols=4)
+    })
+  })
+  
+  ####Country plot####
+  observeEvent(input$type_case_country,{
+    
+    # print(Region_Cases_k)
+    maxnewcases= 0
+    # for (k in Countrylist){
+    location = "Portugal"
+    if (input$type_case_country == "Total"){
+      Country_Cases_k = Country_Cases[,c("data",location)]
+    } else if(input$type_case_country == "Deaths"){
+      Country_Cases_k = Country_Deaths[,c("data",location)]
+    } else if(input$type_case_country == "Active"){
+      Country_Cases_k = Country_Active[,c("data",location)]
+    }
+    
+    date=Country_Cases_k$data
     date=as.Date(x=date,"%d-%m-%y")
-    TotalInfected = Region_Cases_k[,c(location)]
+    TotalInfected = Country_Cases_k[,c(location)]
     newcases=TotalInfected
     smooth_newcases=newcases
     smooth_newcases_2=newcases
@@ -179,53 +310,102 @@ server <- function(input, output) {
       newcases[i+1] = max(TotalInfected[i+1]-TotalInfected[i],0)
       newcases[is.na(newcases)]=0
       newcases[newcases<0]=0
-      smooth_newcases[i+1] = max((TotalInfected[i+1]-TotalInfected[max(1,i-rollingaverage-1)])/min(i,rollingaverage),0)
+      if(input$type_case_country != "Active"){
+        smooth_newcases[i+1] = max((TotalInfected[i+1]-TotalInfected[max(1,i-rollingaverage-1)])/min(i,rollingaverage),0)
+      } else {
+        smooth_newcases[i+1] = TotalInfected[i+1]
+        }
     }
     maxnewcases=max(maxnewcases,smooth_newcases)
     
+    plotdata_country=data.frame(date,smooth_newcases,newcases)
     
-    plotdata_region=data.frame(date,smooth_newcases,newcases)
-    
-    colour = if(smooth_newcases[length(smooth_newcases)] < 10){
-      "#52854C"
-    } else if(smooth_newcases[length(smooth_newcases)]/max(smooth_newcases)<0.2 || smooth_newcases[length(smooth_newcases)] < 20){
-      "#E7B800"
-    } else {
-      "#FC4E07"
+    if (input$type_case_country == "Total"){
+      colour = if(smooth_newcases[length(smooth_newcases)] < 100){
+        "#52854C"
+      } else if(smooth_newcases[length(smooth_newcases)]/max(smooth_newcases)<0.2 || smooth_newcases[length(smooth_newcases)] < 200){
+        "#E7B800"
+      } else {
+        "#FC4E07"
+      }
+      output$newcasesplot_country <- renderPlot({
+        p <- ggplot(plotdata_country,aes(x=date)) +
+          geom_line(aes(y=smooth_newcases), size = 2, color=colour) +
+          ggtitle(paste0(location," (total cases: ",TotalInfected[length(TotalInfected)],")")) +
+          xlab ("") + ylab("Daily cases") + ylim(0, max(smooth_newcases,10)) +
+          annotate(
+            geom = "curve",
+            x = confinamentostart,
+            y = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentostart)],
+            xend = confinamentostart-2,
+            yend = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentostart)]+100, 
+            curvature = 0, arrow = arrow(length = unit(2, "mm"))) +
+          annotate(
+            geom = "text",
+            x = confinamentostart-2,
+            y = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentostart)]+120,
+            label = "Lockdown start",
+            hjust = "right"
+          ) +
+          annotate(
+            geom = "curve",
+            x = confinamentoend,
+            y = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentoend)],
+            xend = confinamentoend+2,
+            yend = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentoend)]+100, 
+            curvature = 0, arrow = arrow(length = unit(2, "mm"))) +
+          annotate(
+            geom = "text",
+            x = confinamentoend+2,
+            y = plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentoend)]+120,
+            label = "Lockdown end",
+            hjust = "left"
+          )
+          theme(axis.text=element_text(face="bold"),
+                axis.title=element_text(face="bold"),
+                aspect.ratio = 1/1.618,
+                plot.title = element_text(face = "bold"))
+        p
+        # print(plotdata_country$smooth_newcases[which(plotdata_country$date == confinamentostart)])
+      })
+    } else if(input$type_case_country == "Deaths"){
+      colour = if(smooth_newcases[length(smooth_newcases)] < 10){
+        "#52854C"
+      } else if(smooth_newcases[length(smooth_newcases)] < 20){
+        "#E7B800"
+      } else {
+        "#FC4E07"
+      }
+      output$newcasesplot_country <- renderPlot({
+        p <- ggplot(plotdata_country,aes(x=date)) +
+          geom_line(aes(y=smooth_newcases), size = 2, color=colour) +ggtitle(paste0(location," (total deaths: ",TotalInfected[length(TotalInfected)],")")) +
+          xlab ("") + ylab("Daily deaths") + ylim(0, max(smooth_newcases,1)) +
+          theme(axis.text=element_text(face="bold"),
+                axis.title=element_text(face="bold"),
+                aspect.ratio = 1/1.618,
+                plot.title = element_text(face = "bold"))
+        p
+      })
+    } else if(input$type_case_country == "Active"){
+      colour = if(smooth_newcases[length(smooth_newcases)] < 100){
+        "#52854C"
+      } else if(smooth_newcases[length(smooth_newcases)]/max(smooth_newcases)<0.2 || smooth_newcases[length(smooth_newcases)] < 200){
+        "#E7B800"
+      } else {
+        "#FC4E07"
+      }
+      output$newcasesplot_country <- renderPlot({
+        p <- ggplot(plotdata_country,aes(x=date)) +
+          geom_line(aes(y=smooth_newcases), size = 2, color=colour) +ggtitle(paste0(location," (active cases: ",TotalInfected[length(TotalInfected)],")")) +
+          xlab ("") + ylab("Active cases") + ylim(0, max(smooth_newcases,1)) +
+          theme(axis.text=element_text(face="bold"),
+                axis.title=element_text(face="bold"),
+                aspect.ratio = 1/1.618,
+                plot.title = element_text(face = "bold"))
+        p
+      })
     }
-    
-    
-    p <- ggplot(plotdata_region,aes(x=date)) +
-      geom_line(aes(y=smooth_newcases), size = 2, color=colour) +
-      # geom_point(aes(y=newcases), size = 3, color=colour) +
-      ggtitle(paste0(location," (total cases: ",TotalInfected[length(TotalInfected)],")")) +
-      xlab ("") + ylab("Daily cases") +
-      theme(axis.text=element_text(size=14,face="bold"),
-            axis.title=element_text(size=16,face="bold"),
-            aspect.ratio = 1/1.618,
-            plot.title = element_text(size = 20, face = "bold"))
-    if (k == "Norte"){
-      p1 = p
-    } else if (k == "Centro"){
-      p2 = p
-    } else if (k == "Lisboa e Vale do Tejo"){
-      p3 = p
-    } else if (k == "Alentejo"){
-      p4 = p
-    } else if (k == "Algarve"){
-      p5 = p
-    } else if (k == "Acores"){
-      p6 = p
-    } else if (k == "Madeira"){
-      p7 = p
-    }
-  }
-  output$newcasesplot_region <- renderPlot({
-
-    
-    multiplot(p1, p2, p3, p4, p5, p6, p7, cols=4)
   })
-  # })
 }
 
 shinyApp(ui, server)
